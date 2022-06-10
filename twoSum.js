22bf6d1fdf1134360d8f66353d634c1a2b6d8b8b
Rose Reyes <36457350+RoseReyes@users.noreply.github.com>
2022-06-10 09:29:45 -0700

    Add files via upload
    Removing uncomment code

--- twoSum.js
+++ twoSum.js
@@ -1,42 +1,44 @@
 // // brute force time coding plus testing the code is 45 mins
 // // time complexity is o(n^2)
 
-// const twoSum = (nums, target) => {
-//     for (var i = 0; i < nums.length; i++) {
-//         for (var j = i + 1; j < nums.length; j++) {
-//             if (nums[i] + nums[j] === target) {
-//                 return [i, j];
-//             }
-//         }
-//     }
-// }
+const twoSum = (nums, target) => {
+    for (var i = 0; i < nums.length; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+}
 
-// twoSum([2,3,4,5,11], 7); // output: [1,2]
-// twoSum([2,3,4,5,11], 9); // output [2,3]
-// twoSum([3,2,4], 6); // output [1,2]
-// twoSum([2,2], 4); // output [0,1]
+twoSum([2,3,4,5,11], 7); // output: [1,2]
+twoSum([2,3,4,5,11], 9); // output [2,3]
+twoSum([3,2,4], 6); // output [1,2]
+twoSum([2,2], 4); // output [0,1]
 
 
-// // optimized by using hashmap/object/dictionary
-// // time complexity O(n)and space complexity of O(1)
+// optimized by using hashmap/object/dictionary
+// time complexity O(n)and space complexity of O(1)
 
-// const twoSumOptimized = (nums, target) => {
-//     let hashmap = {};
+const twoSumOptimized = (nums, target) => {
+    let hashmap = {};
 
-//     for(let o = 0; o < nums.length; o++) {
-//         let currentValue = nums[o];
-//         let complement = target - currentValue;
+    for(let o = 0; o < nums.length; o++) {
+        let currentValue = nums[o];
+        let complement = target - currentValue;
 
-//         if (hashmap[complement] !== undefined)
-//             return [hashmap[complement], o];
-//         else 
-//             hashmap[nums[o]] = o;
-//     }
-// }
+        if (hashmap[complement] !== undefined)
+            return [hashmap[complement], o];
+        else 
+            hashmap[nums[o]] = o;
+    }
+}
 
-// console.log(twoSumOptimized([2,3,4,5,11], 9));
+console.log(twoSumOptimized([2,3,4,5,11], 9));
 
 // using two-pointer approach
+// time complexity O(n) since we only loop through the array once
+
 const twoSumTwoPointer = (nums, target) => {
     let leftPointer = 0;
     let rightPointer = nums.length - 1;
